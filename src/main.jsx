f4f4b2a32b67fc2bc4f0dca7fd95894c7d25dc22
Mateus Prado <mateus.prado@cyncly.com>
2025-07-28 23:48:47 -0300

    Ajustes

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './components/App.jsx'
 import './assets/scss/app.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInstagram, faReddit } from '@fortawesome/free-brands-svg-icons';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
